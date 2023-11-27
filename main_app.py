246819f7b52651d0a3e3d76165786a27f4fc82c5
MarkoLeskovar <markic1996@gmail.com>
2023-11-27 23:06:14 +0100

    Deleted exe file

--- main_app.py
+++ main_app.py
@@ -1,9 +1,8 @@
-# Import the main app
 from fractals.fractal_render import FractalRenderingApp
 
 # Main function call
 if __name__ == '__main__':
-    app = FractalRenderingApp(window_size=(800, 450),
+    app = FractalRenderingApp(
         config_file='fractals/assets/config.txt',
     )
     app.Run()
